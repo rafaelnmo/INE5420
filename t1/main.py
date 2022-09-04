85c7ff1c46160711e7708f1c65ab4b99c6c81b5c
rafaelnmo <rafaelnevesmello@gmail.com>
2022-09-04 19:56:51 -0300

    Adding initial main.py file

---
+++ t1/main.py
@@ -0,0 +1,7 @@
+"""
+Essa é uma aplciação que permite desenhar pontos, retas e polígonos. Além disso, ela também 
+permite movimentar e dar zoom na viewport.
+
+Autor: Rafael Oliveira e Andre Casagranda
+Curso: INE5420 - Computacao Grafica
+"""
