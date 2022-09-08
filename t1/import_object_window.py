87910765decd6238d7adfb3019c05b8bbe8d271b
Andre <casagrandamerlo@gmail.com>
2022-09-08 13:13:39 -0300

    ajustada tela para solicitar informacoes do ponto

--- t1/import_object_window.py
+++ t1/import_object_window.py
@@ -1,11 +1,13 @@
-from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout, QLabel, QLineEdit)
+from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout, QLabel, QLineEdit, QHBoxLayout, QSpinBox)
 
 class ImportObjectWindow(QDialog):
     def __init__(self, parent=None, obj="Objeto"):
         super().__init__()
 
         self.setWindowTitle("Incluir {0}".format(obj))
-        self.nameLineEdit = QLineEdit()
+        self.nameEdit = QLineEdit()
+        self.x1Edit = QSpinBox()
+        self.y1Edit = QSpinBox()
         self.parent = parent
 
         QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
@@ -19,7 +21,15 @@ class ImportObjectWindow(QDialog):
   
         # adding rows
         # for name and adding input text
-        layout.addRow(QLabel("Nome do {0}: ".format(obj)), self.nameLineEdit)
+        layout.addRow(QLabel("Nome do {0}: ".format(obj)), self.nameEdit)
+
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(QLabel("X:"))
+        hLayout.addWidget(self.x1Edit)
+        hLayout.addWidget(QLabel("Y:"))
+        hLayout.addWidget(self.y1Edit)
+
+        layout.addRow(hLayout)
 
         layout.addWidget(buttonBox)
         self.setLayout(layout)
@@ -27,8 +37,12 @@ class ImportObjectWindow(QDialog):
     def getInfo(self):
   
         # printing the form information
-        name = self.nameLineEdit.text()
+        name = self.nameEdit.text()
+        x = self.x1Edit.text()
+        y = self.y1Edit.text()
         print("Object Name: {0}".format(name))
+        print("Object X: {0}".format(x))
+        print("Object Y: {0}".format(y))
         self.parent.import_object(name)
   
         # closing the window
